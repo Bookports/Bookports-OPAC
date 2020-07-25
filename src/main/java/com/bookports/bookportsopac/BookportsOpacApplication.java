091ec3715b416fb62fdb4d4a9a8d44e7c8b1387e
Dom Rodriguez <shymega@shymega.org.uk>
2020-07-26 00:03:53 +0100

    Initial commit

---
+++ src/main/java/com/bookports/bookportsopac/BookportsOpacApplication.java
@@ -0,0 +1,13 @@
+package com.bookports.bookportsopac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookportsOpacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookportsOpacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookports/bookportsopac/BookportsOpacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookports.bookportsopac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookportsOpacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
